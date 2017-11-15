0532fcaa4ebe3915e7825443924b3cde23aba5dc
S Z <zutshisunakshi@gmail.com>
2017-11-14 21:24:40 -0600

    update Home page css

--- Home.php
+++ Home.php
@@ -3,8 +3,15 @@
 <head>
 <title>Home Page Portal : Image manupulation to b/w</title>
 </head>
+<style>
+body
+{
+    background-image:url('background.jpg');
+}
+</style>
 <body>
-  <h2>Add image to your bucket: Click below to begin</h2>
+  <h1 align="center">Add image to your bucket: Click below to begin</h1>
+    <h2 align="center">ITMO544 - MP1</h2>
 
   <a href="Submit.php">Click to submit image</a>
 

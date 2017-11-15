427015042a0ea7764fe22c1920adee4e9e3cabfb
S Z <zutshisunakshi@gmail.com>
2017-11-14 23:21:44 -0600

    updated the Home and submit php pages

--- Home.php
+++ Home.php
@@ -3,18 +3,14 @@
 <head>
 <title>Home Page Portal : Image manupulation to b/w</title>
 </head>
-<style>
-body
-{
-    background-image:url('background.jpg');
-    background-repeat: no-repeat;
-}
-</style>
+
 <body>
   <h1 align="center">Add image to your bucket: Click below to begin</h1>
     <h2 align="center">ITMO544 - MP1</h2>
 
-  <a href="Submit.php">Click to submit image</a>
+  <h3 align="center"><a href="Submit.php">Click to submit image</a></h3>
+
+  <img src="background.jpg" alt="Gallery background animated" width="800" height="600" />
 
 </body>
 </html>

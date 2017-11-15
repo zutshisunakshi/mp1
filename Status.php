b90093126745734a2159bdfc97b7849b8446829d
S Z <zutshisunakshi@gmail.com>
2017-11-15 14:06:14 -0600

    updated Status page code

--- Status.php
+++ Status.php
@@ -1,17 +1,19 @@
 <?php
+echo "<h2>Your Inputs:</h2>";
+echo $_POST["email"];
+echo "<br>";
+echo $_POST["phone];
+echo "<br>";
+echo $_POST["userfile];
+echo "<br>";
+
+?>
+
+<?php
 // Start the session
 session_start();
 // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 // of $_FILES.
-
-echo "<h2>Your Inputs:</h2>";
-echo $email;
-echo "<br>";
-echo $phone;
-echo "<br>";
-echo $userfile;
-echo "<br>";
-<hr>
 
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);

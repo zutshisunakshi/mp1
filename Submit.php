fcc41fd00118250294858d684c82e169c4e58afa
S Z <zutshisunakshi@gmail.com>
2017-11-15 16:07:28 -0600

    update the MP1 Submit page email validation

--- Submit.php
+++ Submit.php
@@ -20,16 +20,13 @@ function check(o) {
     }
 }
 
-function checkEmail() {
-
-    var email = document.getElementById('Email');
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-    if (!filter.test(email.value)) {
-    alert('Please provide a valid email address');
-    email.focus;
-    return false;
- }
+function emailcheck(o) {
+    var re = /([A-Z0-9a-z_-][^@])+?@[^$#<>?]+?\.[\w]{2,4}/.test(o.value);
+    if(!re) {
+      $('#error').show();
+    } else {
+     $('#error').hide();
+    }
 }
 </script>
 </head>
@@ -39,13 +36,14 @@ function checkEmail() {
   <h2 align="center">Add image to your bucket form</h2>
   <p><span class="error">All are required fields</span></p>
   <form enctype="multipart/form-data" action="Status.php" method="POST">
-    <div>  Email ID <input id="Email" type="text" name="email" placeholder="Enter email..."></div>
+    <div>  Email ID <input onkeyup="emailcheck(this)" type="text" name="email" placeholder="Enter email..."></div>
+    <span id="error" style="display:none;color:red;">Wrong email</span>
       <br><br>
     <div>  Cell no. <input type="text" maxlength="10" onkeyup="check(this)" name="phone" placeholder="Enter phone no..."></div>
       <br><br>
     <div>  <input name="userfile" type="file" /></div>
       <br><br>
-    <div>  <input class="btn btn-success btn-send" type="submit" name="submit" value="Submit" onclick='Javascript:checkEmail();'></div>
+    <div>  <input class="btn btn-success btn-send" type="submit" name="submit" value="Submit"></div>
 </div>
 
   <hr>
@@ -54,8 +52,8 @@ function checkEmail() {
   <!-- The data encoding type, enctype, MUST be specified as below -->
   <form enctype="multipart/form-data" action="Gallery.php" method="POST">
 
-  View your uploads: <input id="Email" type="email" name="email">
-  <input class="btn btn-success btn-send" type="submit" value="View Gallery" onclick='Javascript:checkEmail();'/>
+  View your uploads: <input type="email" name="email">
+  <input class="btn btn-success btn-send" type="submit" value="View Gallery" />
   </form>
 </div>
 

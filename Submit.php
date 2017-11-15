41304403c5b8aba341a226474c5bb88d13704953
S Z <zutshisunakshi@gmail.com>
2017-11-15 00:13:32 -0600

    updated the Submit page

--- Submit.php
+++ Submit.php
@@ -2,35 +2,38 @@
 <html>
 <head>
 <title>Customer Portal : Image manupulation to b/w</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 <style>
  .error {color: #FF0000;}
 </style>
 </head>
 <body>
 
-
-<h2>Add image to your bucket!!</h2>
-<p><span class="error">* required field.</span></p>
-<form enctype="multipart/form-data" action="Status.php" method="POST">
-    E-mail: <input type="text" name="email">
-    <br><br>
-    Phone No.: <input type="text" name="phone">
-    <br><br>
-    Add this Image file: <input name="userfile" type="file" />
-    <br><br>
-    <input type="submit" name="submit" value="Submit">
-</form>
-
+<div class="container">
+  <h2 align="center">Add image to your bucket form</h2>
+  <p><span class="error">All are required fields</span></p>
+  <form enctype="multipart/form-data" action="Status.php" method="POST">
+      E-mail: <input type="text" name="email" placeholder="Please enter your email *">
+      <br><br>
+      Cellno.: <input type="text" name="phone" placeholder="Please enter your phone no. *">
+      <br><br>
+      Add this Image file: <input name="userfile" type="file" />
+      <br><br>
+      <input class="btn btn-success btn-send" type="submit" name="submit" value="Submit">
+  </form>
+</div>
 
   <hr />
   <!-- The data encoding type, enctype, MUST be specified as below -->
   <form enctype="multipart/form-data" action="Gallery.php" method="POST">
 
-  Enter Email of user for gallery to browse: <input type="email" name="email">
-  <input type="submit" value="Load Gallery" />
+  View your uploads: <input type="email" name="email">
+  <input class="btn btn-success btn-send" type="submit" value="View Gallery" />
   </form>
 
 
-
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
